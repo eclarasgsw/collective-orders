018bd3c71524614b668ddab04cbf4e62307e96b8
eclarasgsw <102331707+eclarasgsw@users.noreply.github.com>
2023-06-11 22:47:57 +0000

    Added dynamic data

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-
+PRODUCTS = [
+  {
+    'id': 1,
+    'product_name': 'Karotten',
+    'price': '5',
+    'currency': 'Fr.',
+    'unit': 'kg',
+    'producer':'Tübach - Granwehr'
+  },
+    {
+    'id': 2,
+    'product_name': 'Erdbeeren',
+    'price': '4',
+    'currency': 'Fr.',
+    'unit': '250g',
+    'producer':'Gossau - Fust'
+  },
+    {
+    'id': 3,
+    'product_name': 'Salat',
+    'price': '3',
+    'currency': 'Fr.',
+    'unit': 'Stück',
+    'producer':'St.Gallen - Müller'
+  }
+]
 
 @app.route("/")
-def hello_world():
-  return "Hello"
+def hello_order():
+  return render_template('home.html', 
+                         products=PRODUCTS)
+
+@app.route("/api/products")
+def list_products():
+  return jsonify(PRODUCTS)
 
 if __name__ == "__main__":
-  app.run(host= '0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True)
