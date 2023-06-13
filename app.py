f331a0a5dd1075fa4097106bb8ede7d8e9141f3c
eclarasgsw <102331707+eclarasgsw@users.noreply.github.com>
2023-06-13 09:24:42 +0000

    Implemented dictionary

--- app.py
+++ app.py
@@ -2,72 +2,42 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-PRODUCTS = [
-  {
-    'id': 1,
-    'name': 'Karotten',
-    'price': '5',
-    'currency': 'Fr.',
-    'unit': 'kg',
-    'producer':'Tübach - Granwehr'
-  },
-    {
-    'id': 2,
-    'name': 'Erdbeeren',
-    'price': '4',
-    'currency': 'Fr.',
-    'unit': '250g',
-    'producer':'Gossau - Fust'
-  },
-    {
-    'id': 3,
-    'name': 'Salat',
-    'price': '3',
-    'currency': 'Fr.',
-    'unit': 'Stück',
-    'producer':'St.Gallen - Müller'
-  }
-]
+PRODUCTS = [{
+  'id': 1,
+  'name': 'Karotten',
+  'price': '5',
+  'currency': 'Fr.',
+  'unit': 'kg',
+  'producer': 'Tübach - Granwehr'
+}, {
+  'id': 2,
+  'name': 'Erdbeeren',
+  'price': '4',
+  'currency': 'Fr.',
+  'unit': '250g',
+  'producer': 'Gossau - Fust'
+}, {
+  'id': 3,
+  'name': 'Salat',
+  'price': '3',
+  'currency': 'Fr.',
+  'unit': 'Stück',
+  'producer': 'St.Gallen - Müller'
+}]
 
-DEPOTS = [
-  {
-    'id': 1,
-    'name': 'Lachen Drogerie',
-    'street_number': 4,
-    'street': 'fff',
-    'postcode': 9000,
-    'city': 'St.Gallen'
-  },
-  {
-    'id': 2,
-    'name': 'Liva Natura',
-    'street_number': 4,
-    'street': 'fff',
-    'postcode': 9000,
-    'city': 'St.Gallen'
-  },  
-  {
-    'id': 3,
-    'name': 'Benevolpark',
-    'street_number': 4,
-    'street': 'fff',
-    'postcode': 9000,
-    'city': 'St.Gallen'
-  }
-]
+AMOUNT = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-AMOUNT = [1,2,3,4,5,6,7,8,9,10]
 
 @app.route("/")
 def hello_order():
-  return render_template('home.html', 
-                        products=PRODUCTS,
-                        depots=DEPOTS,
-                        amount=AMOUNT)
+  return render_template('home.html', products=PRODUCTS, amount=AMOUNT)
+
 
 @app.route("/api/products")
 def list_products():
   return jsonify(PRODUCTS)
+
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,17 +1,17 @@
 from sqlalchemy import create_engine, text
+import os
 
-db_connection_string = "mysql+pymysql://frbqbicyh7cod7fj4n25:pscale_pw_hCDpD8vD51OB1x5YVJ5feUCNFgtGc6nsUHtEu0ye9hG@aws.connect.psdb.cloud/collective-orders?charset=utf8mb4"
+my_secret = os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(
-  db_connection_string,
-  connect_args={
-        "ssl": {
-            "ssl_ca": "/etc/ssl/cert.pem"
-                }
-              }           
-)
+engine = create_engine(my_secret,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
 with engine.connect() as conn:
   result = conn.execute(text("select * from depots"))
-  firstresult=result.all()[0]
-  print(firstresult)
+
+  depots = []
+  for row in result.all():
+    depots.append(row._mapping)
+  print(depots)
